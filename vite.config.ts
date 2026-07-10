54bf875744eb62c1c79cff0cfc7b5cc8b4318f2e
Cunnekba <cuongquang2006@gmail.com>
2026-07-11 00:40:30 +0700

    f : fix formatting and add base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/image-to-pdf/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/app'),
     },
   },
-})
+});
